8c440211bbf9b75365070d4276f1a1a06b97d85a
Brian Stafford <contact@brianstafford.info>
2020-10-15 17:46:49 +0100

    Add features
    * XML render
    * get list of links
    * update links from dictionary
    * add option argument to constructor
    * add licence and flesh out README.md

--- README.md
+++ README.md
@@ -1,3 +1,71 @@
-# PyCMark
+# Halcyon CMark
 
-# Just a simple python interface to call the cmark-gfm API.
+Halcyon CMark a simple python interface to call the
+[cmark-gfm](https://github.com/github/cmark-gfm) API.
+
+Unlike most other Python modules to interface to Commonmark, this one does not
+make you use a modified version of cmark-gfm, just install the version that is
+packaged by your distribution or build the version from GitHub.
+
+Halcyon because it is simple and frustration free.
+
+## Install
+
+This is straightforward. Download source, make sure cmark-gfm is installed,
+then:
+
+```sh
+$ python setup.py build
+$ sudo python setup.py install
+```
+
+Python3 is assumed.  This might build for Python2.7 but I have not checked this.
+
+## Copyright and Licence
+
+This software is © 2020 Brian Stafford and the licence is
+[LGPL 2.1](https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html).
+
+## Usage
+
+```python
+import cmark
+
+markdown='''# Hello *World*
+
+Just a quick test!
+'''
+
+md = cmark.CMark(markdown)
+print(md.render_html())
+```
+
+produces 
+
+```html
+<h1>Hello <em>World</em></h1>
+<p>Just a quick test!</p>
+```
+
+## Methods
+
+Constructor/Method | Return | Description
+---:|:---:|:---
+CMark(text, options=_defaults_) | instance | Read and parse markdown document.
+render_html() | str | Render parsed document as HTML.
+render_xml() | str | Render parsed document as Commonmark XML.
+links() | tuple | Return a sequence of (url, title) tuples for each link in the document.
+update_links(dict) | | Update links in document from a dictionary, where the key is the URL to update and value is the replacement.
+
+The `links()` and `update_links()` methods can be used to fix up links
+before rendering the document. For example:
+
+```python
+import cmark
+markdown = '...'
+
+md = cmark.CMark(markdown)
+print(md.links())
+md.update_links({'example.md': 'example.html'})
+print(md.render_html())
+```
--- pycmark/module.c
+++ pycmark/module.c
@@ -1,14 +1,35 @@
+/*
+ *  This file is part of halcyon-cmark, a Python3 interface to the
+ *  Github Flavored Commonmark C library.
+ *
+ *  Copyright (C) 2020  Brian Stafford
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 #include <cmark-gfm.h>
 #include <cmark-gfm-extension_api.h>
 
+/* these don't appear in the cmark-gfm headers */
 cmark_syntax_extension *create_table_extension ();
 cmark_syntax_extension *create_strikethrough_extension ();
 cmark_syntax_extension *create_tagfilter_extension ();
 cmark_syntax_extension *create_autolink_extension ();
 
-/* https://docs.python.org/3.8/extending/newtypes_tutorial.html */
 typedef struct _CMarkObject CMarkObject;
 struct _CMarkObject
   {
@@ -23,7 +44,7 @@ cmark_dealloc (CMarkObject *self)
 {
   cmark_node_free (self->document);
 
-  Py_TYPE(self)->tp_free((PyObject *) self);
+  Py_TYPE(self)->tp_free ((PyObject *) self);
 }
 
 static int
@@ -33,19 +54,25 @@ cmark_init (CMarkObject *self, PyObject *args, PyObject *kwds)
   Py_ssize_t len;
   cmark_parser *parser;
   cmark_syntax_extension *syntax_extension;
+  static char *keywords[] =
+    {
+      "text",
+      "options",
+    };
+  int options = CMARK_OPT_SMART
+		| CMARK_OPT_FOOTNOTES
+		| CMARK_OPT_TABLE_PREFER_STYLE_ATTRIBUTES
+		| CMARK_OPT_FULL_INFO_STRING;
 
-  if (!PyArg_ParseTuple (args, "s#", &text, &len))
+  if (!PyArg_ParseTupleAndKeywords (args, kwds, "s#|I", keywords,
+				    &text, &len, &options))
     return -1;
 
-  self->options =   CMARK_OPT_SMART
-		  | CMARK_OPT_FOOTNOTES
-		  | CMARK_OPT_STRIKETHROUGH_DOUBLE_TILDE
-		  | CMARK_OPT_TABLE_PREFER_STYLE_ATTRIBUTES
-		  | CMARK_OPT_FULL_INFO_STRING
-		  ;
+  self->options = options;
 
-  parser =  cmark_parser_new (self->options);
+  parser = cmark_parser_new (self->options);
 
+  //FIXME - make extensions searchable, pass as argument!
   //syntax_extension = cmark_find_syntax_extension(argv[i]);
   syntax_extension = create_table_extension ();
   cmark_parser_attach_syntax_extension (parser, syntax_extension);
@@ -75,15 +102,125 @@ cmark_html (CMarkObject *self, PyObject *Py_UNUSED(ignored))
   return tail;
 }
 
-static PyMethodDef cmark_methods[] = {
+static PyObject *
+cmark_xml (CMarkObject *self, PyObject *Py_UNUSED(ignored))
+{
+  PyObject *tail;
+  char *text;
+
+  text = cmark_render_xml (self->document, self->options);
+  tail = PyUnicode_FromString (text);
+  free (text);
+  return tail;
+}
+
+/* Return sequence of 2 tuples
+	((url, title), ...)
+   for each link in the document */
+static PyObject *
+cmark_links (CMarkObject *self, PyObject *Py_UNUSED(ignored))
+{
+  cmark_event_type ev_type;
+  cmark_iter *iter;
+  cmark_node *node;
+  PyObject *tuple, *item, *url, *title;
+  Py_ssize_t len = 0, elements = 20;
+
+  tuple = PyTuple_New (elements);
+
+  iter = cmark_iter_new (self->document);
+  while ((ev_type = cmark_iter_next (iter)) != CMARK_EVENT_DONE)
     {
-      "render_html",
-      (PyCFunction) cmark_html,
-      METH_NOARGS,
-      ""
-    },
-    { NULL }
-};
+      if (ev_type != CMARK_EVENT_ENTER)
+	continue;
+      node = cmark_iter_get_node (iter);
+      switch (cmark_node_get_type (node))
+	{
+	case CMARK_NODE_LINK:
+	  url = PyUnicode_FromString (cmark_node_get_url (node));
+	  title = PyUnicode_FromString (cmark_node_get_title (node));
+	  item = PyTuple_Pack (2, url, title);
+	  if (len >= elements)
+	    {
+	      elements = len + 10;
+	      _PyTuple_Resize (&tuple, elements);
+	    }
+	  PyTuple_SET_ITEM (tuple, len++, item);
+	  break;
+	default:
+	  break;
+	}
+    }
+  cmark_iter_free (iter);
+
+  _PyTuple_Resize (&tuple, len);
+  return tuple;
+}
+
+/* Update links in parsed document.  The argument should be a dict() object
+   with the URL to be replaced as the key and replacement URL as value. */
+static PyObject *
+cmark_update_links (CMarkObject *self, PyObject *dict)
+{
+  cmark_event_type ev_type;
+  cmark_iter *iter;
+  cmark_node *node;
+  PyObject *item;
+  const char *url;
+
+  if (!PyDict_Check (dict))
+    return NULL;
+
+  iter = cmark_iter_new (self->document);
+  while ((ev_type = cmark_iter_next (iter)) != CMARK_EVENT_DONE)
+    {
+      if (ev_type != CMARK_EVENT_ENTER)
+	continue;
+      node = cmark_iter_get_node (iter);
+      switch (cmark_node_get_type (node))
+	{
+	case CMARK_NODE_LINK:
+	  item = PyDict_GetItemString (dict, cmark_node_get_url (node));
+	  if (item != NULL && PyUnicode_Check (item))
+	    {
+	      url = PyUnicode_AsUTF8 (item);
+	      cmark_node_set_url (node, url);
+	    }
+	  break;
+	default:
+	  break;
+	}
+    }
+  cmark_iter_free (iter);
+
+  Py_INCREF (Py_None);
+  return Py_None;
+}
+
+static PyMethodDef cmark_methods[] =
+  {
+    {
+     "render_html",
+     (PyCFunction) cmark_html,
+     METH_NOARGS,
+     "Render the parse tree as HTML"},
+    {
+     "render_xml",
+     (PyCFunction) cmark_xml,
+     METH_NOARGS,
+     "Render the parse tree as XML"},
+    {
+     "links",
+     (PyCFunction) cmark_links,
+     METH_NOARGS,
+     "Return a sequence of 2-tuples with link (url, title)"},
+    {
+     "update_links",
+     (PyCFunction) cmark_update_links,
+     METH_O,
+     "Update links in parsed document from dictionary; ie newurl = dict[url]"},
+    {NULL}
+  };
 
 static PyTypeObject CMarkType =
   {
@@ -108,7 +245,7 @@ static struct PyModuleDef moduledef =
     .m_size = -1,
   };
 
-void cmark_gfm_core_extensions_ensure_registered(void);
+void cmark_gfm_core_extensions_ensure_registered (void);
 
 PyMODINIT_FUNC
 PyInit_cmark (void)
@@ -128,6 +265,28 @@ PyInit_cmark (void)
       Py_DECREF (module);
       return NULL;
     }
+
+  PyModule_AddIntConstant (module, "OPT_DEFAULT", CMARK_OPT_DEFAULT);
+  PyModule_AddIntConstant (module, "OPT_SOURCEPOS", CMARK_OPT_SOURCEPOS);
+  PyModule_AddIntConstant (module, "OPT_HARDBREAKS", CMARK_OPT_HARDBREAKS);
+  PyModule_AddIntConstant (module, "OPT_SAFE", CMARK_OPT_SAFE);
+  PyModule_AddIntConstant (module, "OPT_UNSAFE", CMARK_OPT_UNSAFE);
+  PyModule_AddIntConstant (module, "OPT_NOBREAKS", CMARK_OPT_NOBREAKS);
+  PyModule_AddIntConstant (module, "OPT_NORMALIZE", CMARK_OPT_NORMALIZE);
+  PyModule_AddIntConstant (module, "OPT_VALIDATE_UTF8",
+			   CMARK_OPT_VALIDATE_UTF8);
+  PyModule_AddIntConstant (module, "OPT_SMART", CMARK_OPT_SMART);
+  PyModule_AddIntConstant (module, "OPT_GITHUB_PRE_LANG",
+			   CMARK_OPT_GITHUB_PRE_LANG);
+  PyModule_AddIntConstant (module, "OPT_LIBERAL_HTML_TAG",
+			   CMARK_OPT_LIBERAL_HTML_TAG);
+  PyModule_AddIntConstant (module, "OPT_FOOTNOTES", CMARK_OPT_FOOTNOTES);
+  PyModule_AddIntConstant (module, "OPT_STRIKETHROUGH_DOUBLE_TILDE",
+			   CMARK_OPT_STRIKETHROUGH_DOUBLE_TILDE);
+  PyModule_AddIntConstant (module, "OPT_TABLE_PREFER_STYLE_ATTRIBUTES",
+			   CMARK_OPT_TABLE_PREFER_STYLE_ATTRIBUTES);
+  PyModule_AddIntConstant (module, "OPT_FULL_INFO_STRING",
+			   CMARK_OPT_FULL_INFO_STRING);
 
   cmark_gfm_core_extensions_ensure_registered ();
 
